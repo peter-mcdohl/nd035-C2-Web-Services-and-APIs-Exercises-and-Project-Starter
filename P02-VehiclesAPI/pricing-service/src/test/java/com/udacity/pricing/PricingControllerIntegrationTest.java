95d23459e39be3f898584e09658c303488258fbf
Peter E <peter_mcdohl@yahoo.com>
2021-05-15 10:07:15 +0700

    Convert the Pricing Service

---
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.domain.price.Price;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getPrice() {
+
+        for (int i = 1; i < 20; i++) {
+            ResponseEntity<Price> response = this.restTemplate.getForEntity("http://localhost:"+port+"/services/price?vehicleId="+String.valueOf(i), Price.class);
+
+            assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+            assertThat(response.getBody().getVehicleId(), equalTo(Long.valueOf(i)));
+        }
+    }
+}
